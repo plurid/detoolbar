2fddf6560f8c9131175862c86fd747c34fe5a4fc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-19 10:08:44 +0300

    detoolbar-react > setup: package

---
+++ packages/detoolbar-react/source/containers/Detoolbar/__stories__/index.stories.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+// import themes from '@plurid/plurid-themes';
+
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+
+import {
+    withKnobs,
+    // boolean,
+    // number,
+    // select,
+    // text,
+} from '@storybook/addon-knobs';
+
+import Detoolbar from '../';
+
+import {
+    DetoolbarTool,
+} from '../../../data/interfaces';
+
+
+
+const tools: DetoolbarTool[] = [
+    {
+        name: 'One',
+        Tool: () => (<div>One</div>),
+    },
+];
+
+// const actions = {
+//     // atClick: action('atClick'),
+// };
+
+// const themeLabel = 'Theme';
+// const computeThemeOptions = () => {
+//     const options: any = {};
+//     for (const key of Object.keys(themes)) {
+//         options[key] = key;
+//     }
+//     return options;
+// }
+// const themeOptions = computeThemeOptions();
+// const defaultThemeValue = 'plurid';
+
+storiesOf(
+    'Detoolbar',
+    module,
+)
+.addDecorator(withKnobs)
+.add('basic', () => {
+    // const theme = select(themeLabel, themeOptions, defaultThemeValue);
+
+    return (
+        <>
+            <Detoolbar
+                tools={tools}
+
+                // {...actions}
+                // theme={themes[theme]}
+            />
+        </>
+    );
+});
